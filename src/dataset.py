d49f64ce44bdb3b9216c675d8c9143295c44353e
Jaume Palau <jpalauserrano@gmail.com>
2026-07-13 12:44:29 +0200

    Añade clase Dataset_Train para cargar datos del conjunto de entrenamiento

---
+++ src/dataset.py
@@ -0,0 +1,37 @@
+
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class Dataset_Train(Dataset):
+
+    def __init__(self, csv_path='data/raw/conquerx-b05-lec01-digit-recognizer/train.csv'):
+
+        self.df = pd.read_csv(csv_path)
+        self.labels = self.df.iloc[:, 0].values
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+
+        """      
+        Args: 
+            idx (int): Index of the image and label to get.
+        
+        Returns:
+            (img, label): Transformed image as PyTorch Tensor, label as an int.
+        """
+        row = self.df.iloc[idx]
+        label = row.iloc[0]
+        data = row.iloc[1:].values.reshape(28, 28)
+        data = transforms.ToTensor()(data)
+        
+        return data, label
+
+
+if __name__ == "__main__":
+    dataset = Dataset_Train()
+
+    print(dataset[0])
